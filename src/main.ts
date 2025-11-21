cfe954b82a10dba889fbf0fbe27aa920989eb087
kashif281 <kashif281@example.com>
2025-11-21 08:24:47 +0000

    apso commit

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { configureNestApp } from './config/nest-app.config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
   await configureNestApp(app);
   await app.listen(process.env.APP_PORT || 3000);
 }
