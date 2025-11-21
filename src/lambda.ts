2fe0cddb8aafbb2a14f27cde7a74a479dc762a23
kashif281 <kashif281@example.com>
2025-11-21 15:04:11 +0000

    apso commit

--- src/lambda.ts
+++ src/lambda.ts
@@ -15,5 +15,17 @@ export const handler = async (event, context) => {
     });
   }
 
+  if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+      body: '',
+    };
+  }
+
   return cachedServer(event, context);
 };
