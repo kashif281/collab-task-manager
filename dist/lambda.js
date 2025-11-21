a9f0815984ccaf6d27e343057138d3e32351550e
Apso <apso-bot@mavric.com>
2025-11-21 15:40:22 +0000

    Auto-generate database migrations

--- dist/lambda.js
+++ dist/lambda.js
@@ -7,24 +7,30 @@ const app_module_rest_1 = require("./app.module.rest");
 const nest_app_config_1 = require("./config/nest-app.config");
 let cachedServer;
 const handler = async (event, context) => {
-    if (!cachedServer) {
-        const nestApp = await core_1.NestFactory.create(app_module_rest_1.AppModule);
-        await (0, nest_app_config_1.configureNestApp)(nestApp);
-        await nestApp.init();
-        cachedServer = (0, serverless_express_1.configure)({
-            app: nestApp.getHttpAdapter().getInstance(),
-        });
-    }
     if (event.httpMethod === 'OPTIONS') {
         return {
             statusCode: 200,
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 'Access-Control-Allow-Methods': 'GET,POST,PUT,DELETE,OPTIONS',
-                'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+                'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+                'Access-Control-Max-Age': '86400',
             },
             body: '',
         };
+    }
+    if (!cachedServer) {
+        const nestApp = await core_1.NestFactory.create(app_module_rest_1.AppModule);
+        nestApp.enableCors({
+            origin: '*',
+            methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+            allowedHeaders: ['Content-Type', 'Authorization'],
+        });
+        await (0, nest_app_config_1.configureNestApp)(nestApp);
+        await nestApp.init();
+        cachedServer = (0, serverless_express_1.configure)({
+            app: nestApp.getHttpAdapter().getInstance(),
+        });
     }
     return cachedServer(event, context);
 };
